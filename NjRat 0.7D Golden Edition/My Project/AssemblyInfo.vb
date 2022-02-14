f907ddbeb50749dc6d97b1bb9b72352f4ac27ab1
0MrRoot0 <82186529+0MrRoot0@users.noreply.github.com>
2022-02-14 02:10:55 +0200

    Add files via upload

---
+++ NjRat 0.7D Golden Edition/My Project/AssemblyInfo.vb
@@ -0,0 +1,6 @@
+﻿Imports System
+Imports System.Configuration.Assemblies
+Imports System.Reflection
+
+<Assembly: AssemblyAlgorithmId(AssemblyHashAlgorithm.None)>
+<Assembly: AssemblyVersion("0.0.0.0")>
